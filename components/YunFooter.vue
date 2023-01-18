8a9c388130ac409ad9cd56ebc65b65124a0083e6
Big-Cake-jpg <2577117205@qq.com>
2023-01-18 22:48:27 +0800

    ✨ feat: add moeicp to footer

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -9,5 +9,6 @@ useScriptTag('https://busuanzi.icodeq.com/busuanzi.pure.mini.js')
     <div class="copyright flex justify-center items-center">
       <div i-ri-user-line> </div><span id="busuanzi_value_site_uv" />｜<div i-ri-eye-line> </div><span id="busuanzi_value_site_pv" />
     </div>
+    <a href="https://icp.gov.moe/?keyword=20230105" target="_blank">萌 ICP 备 20230105 号</a>
   </YunFooter>
 </template>
