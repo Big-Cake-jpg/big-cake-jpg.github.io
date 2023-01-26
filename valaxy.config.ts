e802b0c22f0228fb141a809a3d5b0e11ae1f88fd
Big-Cake-jpg <2577117205@qq.com>
2023-01-26 22:25:04 +0800

    🐛 fix(pwa): background color

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -45,6 +45,8 @@ const config: UserValaxyNodeConfig<UserThemeConfig> = {
           short_name: '大蛋糕的烘焙坊',
           description: '也许我们会分别，但我们将永远不会忘记彼此',
           theme_color: '#F2BC57',
+          background_color: '#F2BC57',
+          lang: 'zh-CN',
           icons: [
             {
               src: 'https://r2.cakeskin.tk/avatar.webp',
