964f80e13aa9404170105cca339be31f485a08b8
Big_Cake <2577117205@qq.com>
2023-10-22 04:08:07 +0000

    🔧build(dependencies): bump version

---
+++ components/YunWaline.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import type { WalineInitOptions } from '@waline/client'
+import consola from 'consola'
+
+const customOptions: WalineInitOptions = {
+  serverURL: 'https://waline.lihaoyu.cn',
+  pageview: true,
+  comment: true,
+  search: false,
+  locale: {
+    placeholder: '填写邮箱，可以收到回复通知',
+  },
+  imageUploader: (file) => {
+    consola.info('Upload file: ', file)
+
+    const formData = new FormData()
+    const headers = new Headers()
+
+    formData.append('file', file)
+    headers.append('Authorization', ' Bearer !{API TOKEN}')
+    headers.append('Accept', 'application/json')
+    headers.append('strategy_id', '3')
+
+    return fetch('https://img.lihaoyu.cn/api/v0/upload', {
+      method: 'POST',
+      headers,
+      body: formData,
+    })
+      .then(resp => resp.json())
+      .then(resp => resp.data.links.url)
+  },
+}
+</script>
+
+<template>
+  <WalineClient w="full" :options="customOptions" />
+</template>
