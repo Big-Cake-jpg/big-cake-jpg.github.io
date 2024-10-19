b2f9fd7a885cb7138733e5900f3adc1c10451f86
Big-Cake-jpg <me@lihaoyu.cn>
2024-10-20 00:25:10 +0800

    🦄 refactor: better allowedDomain handling

--- valaxy-theme-custom/App.vue
+++ valaxy-theme-custom/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useHead } from "@unhead/vue";
-import { onMounted } from "vue";
+import { onMounted, onBeforeMount } from "vue";
 import { useAppStore, useSiteConfig } from "valaxy";
 import { useRoute } from "vue-router";
 import { useThemeConfig } from "./composables";
@@ -25,26 +25,45 @@ useHead({
 
 const app = useAppStore();
 
-onMounted(async () => {
-  (app.showLoading = false), await import("@fontsource/noto-sans-sc/400.css");
-
+onBeforeMount(async () => {
   // fuck you reverse proxy
   if (isClient) {
     const allowedDomains = [
-      "www.lihaoyu.cn",
-      "lihaoyu.cn",
-      "big-cake-jpg.github.io",
-      "bigcake-blog.netlify.app",
-      "bigcake-blog.vercel.app",
-      "cf-pages.lihaoyu.cn",
-      "bigcake.cakemc.top",
-      "localhost"
-    ];
+      [119, 119, 119, 46, 108, 105, 104, 97, 111, 121, 117, 46, 99, 110],
+      [108, 105, 104, 97, 111, 121, 117, 46, 99, 110],
+      [
+        98, 105, 103, 45, 99, 97, 107, 101, 45, 106, 112, 103, 46, 103, 105,
+        116, 104, 117, 98, 46, 105, 111,
+      ],
+      [
+        98, 105, 103, 99, 97, 107, 101, 45, 98, 108, 111, 103, 46, 110, 101,
+        116, 108, 105, 102, 121, 46, 97, 112, 112,
+      ],
+      [
+        98, 105, 103, 99, 97, 107, 101, 45, 98, 108, 111, 103, 46, 118, 101,
+        114, 99, 101, 108, 46, 97, 112, 112,
+      ],
+      [
+        99, 102, 45, 112, 97, 103, 101, 115, 46, 108, 105, 104, 97, 111, 121,
+        117, 46, 99, 110,
+      ],
+      [
+        98, 105, 103, 99, 97, 107, 101, 46, 99, 97, 107, 101, 109, 99, 46, 116,
+        111, 112,
+      ],
+      [108, 111, 99, 97, 108, 104, 111, 115, 116],
+    ].map((domain) => String.fromCharCode(...domain));
+
     const currentDomain = window.location.hostname;
     if (!allowedDomains.includes(currentDomain)) {
-      window.location.href = "https://www.lihaoyu.cn";
+      window.location.href = "https://" + allowedDomains[0];
     }
   }
+});
+
+onMounted(async () => {
+  (app.showLoading = false), 
+  await import("@fontsource/noto-sans-sc/400.css");
 });
 </script>
 <template>
