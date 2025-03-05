fa890926ade6301f78b2ba84565d3213810a9358
Big-Cake-jpg <me@lihaoyu.cn>
2025-03-05 14:54:08 +0800

    🐞 fix: loading screen

--- valaxy-theme-custom/components/_partial/PageLoading.vue
+++ valaxy-theme-custom/components/_partial/PageLoading.vue
@@ -1,7 +1,7 @@
 <template>
   <Transition name="fade">
     <div
-      class="flex justify-center text-center relative flex-col left-0 right-0 min-h-screen items-center z-50 w-full box-border -mt-[45vh] -mb-[45vh] pt-[45vh] pb-[45vh] bg-[var(--bulma-background)]"
+      class="flex justify-center text-center absolute flex-col left-0 right-0 bottom-0 top-0 items-center z-50 bg-[var(--bulma-background)]"
     >
       <p class="text-lg mb-5">{{ t("loading") }}</p>
       <progress class="progress is-small max-w-75" max="100"></progress>
