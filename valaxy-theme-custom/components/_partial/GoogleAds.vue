f0a6d2305c6079506154dfd399e8ef4ee1d21d30
Big-Cake-jpg <me@lihaoyu.cn>
2025-04-10 20:07:46 +0800

    🐳 chore: re-add google adsense

--- valaxy-theme-custom/components/_partial/GoogleAds.vue
+++ valaxy-theme-custom/components/_partial/GoogleAds.vue
@@ -3,46 +3,43 @@
     <div class="p-4">
       <div class="mb-3 text-sm">{{ t("google_ads.title") }}</div>
     </div>
-    <!-- <div v-if="showAds">
-
-    </div>
-    <div v-else class="fixed">
-      <div class="text-center">{{ t("google_ads.unavailable") }}</div>
-    </div> -->
-    <div class="p-4">
-      <component
-        async
-        :is="'script'"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3406300839504314"
-        crossorigin="anonymous"
-      ></component>
-      <ins
-        class="adsbygoogle"
-        style="display: block"
-        data-ad-client="ca-pub-3406300839504314"
-        data-ad-slot="2681266595"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <component :is="'script'">
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </component>
+    <div>
+      <!--<div v-if="hideAds">
+        <div class="text-center pb-10">{{ t("google_ads.unavailable") }}</div>
+      </div>-->
+      <div class="p-4">
+        <component
+          async
+          :is="'script'"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3406300839504314"
+          crossorigin="anonymous"
+        ></component>
+        <ins
+          class="adsbygoogle"
+          style="display: block"
+          data-ad-client="ca-pub-3406300839504314"
+          data-ad-slot="2681266595"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <component :is="'script'">
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </component>
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import axios from "axios";
-import { ref, onMounted } from "vue";
+// import axios from "axios";
+// import { ref, onMounted } from "vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
 
-const showAds = ref(true);
+/* const hideAds = ref(false);
 
 onMounted(async () => {
   const result = await axios.get("https://api.lihaoyu.cn/blog/ip/ischina");
-  if (result.data.data.isChina !== "false") {
-    showAds.value = false;
-  }
-});
+  hideAds.value = result.data.data.isChina;
+}); */
 </script>
